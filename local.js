889639bcad32f38fd7f5abf615b021c160fba790
mrchomik892 <54484477+mrchomik892@users.noreply.github.com>
2019-11-22 16:15:05 +0100

    Create local.js

---
+++ local.js
@@ -0,0 +1,5 @@
+function antipopup() {
+  alert('Nie możesz otwierać pop-upa tej strony. POWERED BY mrchomik892 | LICENSED UNDER CCA 3.0 Unported License[(czyli uznanie autorstwa]');
+  window.close();
+}
+antipopup();
