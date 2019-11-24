257abf7ec5c73e030fac7f2c7398669ee4b6a33e
zdziwionyrysiczeq <54484477+mrchomik892@users.noreply.github.com>
2019-11-24 10:05:46 +0100

    Update local.js

--- local.js
+++ local.js
@@ -1,5 +1,4 @@
 function antipopup() {
-  alert('Nie możesz otwierać pop-upa tej strony. POWERED BY mrchomik892 | LICENSED UNDER cc-by-sa-4.0 [(czyli uznanie autorstwa]');
   window.close();
 }
 antipopup();
